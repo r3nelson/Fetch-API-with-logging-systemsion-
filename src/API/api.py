b8cd7e5239448940808bf5cc68d84cc05ad7fa53
r3nelson <r3nelson@outlook.com>
2024-04-05 23:49:48 -1000

    changes

--- src/API/api.py
+++ src/API/api.py
@@ -9,7 +9,8 @@ app = FastAPI()
 # need to change to string for easier viewing
 @app.get("/logs/all")
 def display_all_logs():
-    log_lines = read_logfile('../../logs/manga/manga.log')
+    log_lines = read_logfile('manga.log')
+    # log_lines = read_logfile('../../logs/manga/manga.log')
     all_logs = get_all_logs(log_lines)
 
     # reformat all_logs because tuples can't be converted to json
@@ -23,7 +24,8 @@ def display_all_logs():
 # returns debug logs as string for easier viewing 
 @app.get("/logs/debug")
 def display_debug_logs():
-    log_lines = read_logfile('../../logs/manga/manga.log')
+    log_lines = read_logfile('manga.log')
+    # log_lines = read_logfile('../../logs/manga/manga.log')
     all_logs = get_all_logs(log_lines)
     debug_logs = get_specific_logs(log_lines, all_logs, "DEBUG")
     return debug_logs
@@ -31,7 +33,8 @@ def display_debug_logs():
 # returns info logs as string for easier viewing 
 @app.get("/logs/info")
 def display_info_logs():
-    log_lines = read_logfile('../../logs/manga/manga.log')
+    log_lines = read_logfile('manga.log')
+    # log_lines = read_logfile('../../logs/manga/manga.log')
     all_logs = get_all_logs(log_lines)
     info_logs = get_specific_logs(log_lines, all_logs, "INFO")
     return info_logs
@@ -39,7 +42,8 @@ def display_info_logs():
 # returns warning logs as string for easier viewing 
 @app.get("/logs/warning")
 def display_warning_logs():
-    log_lines = read_logfile('../../logs/manga/manga.log')
+    log_lines = read_logfile('manga.log')
+    # log_lines = read_logfile('../../logs/manga/manga.log')
     all_logs = get_all_logs(log_lines)
     warning_logs = get_specific_logs(log_lines, all_logs, "WARNING")
     return warning_logs
@@ -47,7 +51,8 @@ def display_warning_logs():
 # returns error logs as string for easier viewing 
 @app.get("/logs/error")
 def display_error_logs():
-    log_lines = read_logfile('../../logs/manga/manga.log')
+    log_lines = read_logfile('manga.log')
+    # log_lines = read_logfile('../../logs/manga/manga.log')
     all_logs = get_all_logs(log_lines)
     error_logs = get_specific_logs(log_lines, all_logs, "ERROR")
     return error_logs
